ff5f3f39ed08dcc49d2793dc8339f3777d9b53e6
Artur Rocha <adsr@poczta.onet.pl>
2025-04-29 23:55:01 +0200

    Remember the last position in a file

--- init.lua
+++ init.lua
@@ -218,6 +218,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Remember the last position in a file
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = '*',
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
