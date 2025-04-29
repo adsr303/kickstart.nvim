d7288ce5dc307ebf3d3727bc6998b8b8a6829cd6
Artur Rocha <adsr@poczta.onet.pl>
2025-04-30 00:28:14 +0200

    Go formatter config

--- init.lua
+++ init.lua
@@ -777,7 +777,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        go = { 'goimports', 'gofmt' },
+        go = { 'gofmt' }, -- only because of `-s`, see below
         python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
