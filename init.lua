435fc03ab0765a3868e244ec5a561fe30efe21d9
Artur Rocha <adsr@poczta.onet.pl>
2025-05-01 10:52:33 +0200

    Hook up Go plugin + left/right for buffers

--- init.lua
+++ init.lua
@@ -204,6 +204,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Extra navigation
+vim.keymap.set('n', '<A-left>', ':bprev<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<A-right>', ':bnext<CR>', { desc = 'Next buffer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -998,7 +1002,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
